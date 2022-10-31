3471a0f4b82b256efd2087445c77b67f4f0ec053
Gabriell Gardner <gabriell.github@gmail.com>
2022-10-30 22:49:59 -0500

    Updated webpage layout and javascript files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', (event) => 
+
+{
+
+    var mail = getElement('email');
+    var msg = document.getElementsByClassName('message');
+
+    document.getElementsByClassName('form-group').addEventListener('submit', function(ele)
+    {
+        ele.preventDefault()
+        msg.setAttribute(`Thank you! Your email adress ${mail} has been added to our mailing list!`)
+
+        if (mail == null)
+        {
+            msg.setAttribute("Please enter a valid email address")
+            return false
+        }
+
+    });
+
+});
